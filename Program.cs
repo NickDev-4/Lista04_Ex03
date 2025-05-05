c20d1c862844e1d5f30504216fd4c88f994ef4b2
NickDev-4 <nickolasfreire.santos@gmail.com>
2025-05-05 08:43:03 -0300

    Lista 4 - Exercicío 3

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+
+string resposta;
+
+Console.WriteLine("** O FLUXOGRAMA DA ENGENHARIA **");
+
+Console.Write("Está se movendo? (Sim ou Não): ");
+resposta = Console.ReadLine();
+
+if (resposta.ToLower() == "sim") {
+    Console.Write("Deveria? (Sim ou Não): ");
+    resposta = Console.ReadLine();
+    resposta.ToLower();
+
+    if (resposta.ToLower() == "sim"){
+        Console.Write("ÓTIMO");
+    }
+
+    else {
+        Console.Write("USE FITA ADESIVA!");
+    }
+}
+
+else {
+    Console.Write("Deveria? (Sim ou Não): ");
+    resposta = Console.ReadLine();
+
+    if (resposta.ToLower() == "não"){
+        Console.Write("ÓTIMO");
+    }
+
+    else {
+        Console.Write("USE WD-40!");
+    }
+}
